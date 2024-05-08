5f38f72466bf89ae204c527272d6d3aa60169cec
kingwilldabeast <willmur@live.com>
2024-05-07 21:23:04 -0500

    completed assignment

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 let button = document.querySelector("#searchButton")
+let infoButton = document.querySelector("#infoButton")
 let pokemonName = document.querySelector("#pokemonName")
-let pokemonImage = document.querySelector("#pokemonImage")
+let pokemonImage1 = document.querySelector("#pokemonImage1")
+let pokemonImage2 = document.querySelector("#pokemonImage2")
+let stat1 = document.querySelector("#stat1")
+let value1 = document.querySelector("#value1")
+let stat2 = document.querySelector("#stat2")
+let value2 = document.querySelector("#value2")
+let stat3 = document.querySelector("#stat3")
+let value3 = document.querySelector("#value3")
 
-button.addEventListener('click', async () => {
-    let input = document.querySelector("#inputBar").value
-    console.log(input)
-    let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${input}`)
-    console.log(response.data.species.name)
-    let pokemonPic = response.data.sprites.back_default
-    let pokemonType = pokemonName
-    pokemonImage.setAttribute('src', pokemonPic)
-    pokemonName.innerText = response.data.species.name
-  })
+button.addEventListener('click', async ()=> {
+  let input = document.querySelector("#inputBar").value
+  if (input == ``) { input = Math.ceil(Math.random()*200) }
 
-//code from earlier
+    let response = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${input}`
+    )
+    console.log(response)
 
-// const getBreeds = async () => {
-//   const dogBreeds  = await axios.get('https://dog.ceo/api/breeds/list/all')
-//   console.log(dogBreeds.data.message)
-//   }
+    let pokemonPic1 = response.data.sprites.other.showdown.front_default
+    let pokemonPic2 = response.data.sprites.other.showdown.back_default
+    pokemonImage1.setAttribute ('src', pokemonPic1)
+    pokemonImage2.setAttribute ('src', pokemonPic2)
+    
+    let pokemonType = response.data.species.name
+    pokemonName.innerText = pokemonType
 
-// getBreeds()
+    value1.innerText = response.data.moves[0].move.name
+    value2.innerText = response.data.moves[1].move.name
+    // value3.innerText = response.data.abilities[0].ability.name
 
 
+})
+
+infoButton.addEventListener('click',openTab)
+  
+function openTab() {
+  window.open("https://faithfulmoms.org/is-pokemon-safe-children-christian-perspective/", "_blank");
+  }
